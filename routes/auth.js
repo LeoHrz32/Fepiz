86d9aea8a5bc16eba89c847a656b24195537cffa
LeoHrz32 <147664820+LeoHrz32@users.noreply.github.com>
2024-04-07 19:32:13 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,14 @@ const { Router } = require('express'); // Importa la función Router de
 // express para crear un router 
 const router = Router(); // Crea una instancia de Router 
 
-const { login } = require('../controllers/auth'); // Importa el controlador 
+const { login } = require('../controllers/authController'); // Importa el controlador 
 // login desde el archivo '../controllers/auth' 
  
 // Define una ruta POST '/login' que utilizará el controlador login 
 router.post('/login', login); 
  
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, PromGet } = 
-require('../controllers/usuario'); // Importa los controladores desde el 
+require('../controllers/user____Controller'); // Importa los controladores desde el 
 // archivo '../controllers/usuario' 
  
 // Define rutas y asigna controladores a cada ruta 
